b0270b71befe36c0a69285dd64837a07016eaf2c
artiimor <amorcillop@gmail.com>
2020-01-25 11:34:23 +0100

    explained the functions that check if we are in a SO :)

--- ptrace.c
+++ ptrace.c
@@ -163,13 +163,16 @@ int ptrace_readmem(pid_t pid, void *addr, void *buf, size_t n)
     return (int)n;
 }
 
-
 /*********************************************************/
 /*********************Some memory stuff*******************/
-/*Copied from narhem, we did a good work, check his stuff*/
+/*Copied from narhem, he did a good work, check his stuff*/
 /************https://github.com/narhen/procjack***********/
 /*********************************************************/
 
+/**
+ * It allows to free a mem_map_entry struct, it only iterates a mem_map_entry
+ * freeing all the stored memory
+ */
 void mem_maps_free(struct mem_map_entry *ent)
 {
     uint32_t *num, i;
@@ -180,20 +183,58 @@ void mem_maps_free(struct mem_map_entry *ent)
     free(num);
 }
 
+/*
+ * So what are we exactly parsing?
+ * Well, str contains the /proc/pid/maps file content.
+ * 
+ * What the hell is here?
+ * Well, there are some rows, each row contains:
+ *  
+ *  0.- ADDRESS:
+ *      Here we have the starting and ending address of the region
+ *      in the process address space
+ *  
+ *  1.- PERMISSIONS:
+ *      In summary read, write, execute and be shared
+ * 
+ *  2.- OFFSET:
+ *      If the region was mapped from a file (using mmap),
+ *      this is the offset where the mapping begins
+ * 
+ *  3.- DEVICE:
+ *      If the region was mapped from a file, 
+ *      this is the major and minor device number (in hex) where 
+ *      the file lives
+ * 
+ *  4.- INODE: 
+ *      If the region was mapped from a file, this is the file number
+ * 
+ *  5.- PATHNAME:
+ *      If the region was mapped from a file, this is the name of the file.
+ * 
+ *  so, we only are going to be using the address, permissions and pathname,
+ *  and it will be stored in our mem_map_entry struct
+ */
+
 static void parse_maps_ent(char *str, struct mem_map_entry *ent)
 {
+
     int i;
     uint64_t start, end;
     char *str2, *token, *saveptr, *tmp;
 
+    /* in this loop we go to one row of the /proc/pid/maps and
+       obtain the information described previously */
     for (i = 0, str2 = str;; str2 = NULL, ++i)
     {
+
         token = strtok_r(str2, " ", &saveptr);
         if (!token)
             break;
 
         switch (i)
         {
+        /*O.- ADDRESS*/
         case 0:
             tmp = strchr(token, '-');
             *tmp++ = 0;
@@ -205,6 +246,7 @@ static void parse_maps_ent(char *str, struct mem_map_entry *ent)
             ent->addr = (void *)start;
             ent->size = end - start;
             break;
+        /*1.- PERMISSIONS*/
         case 1:
             if (token[0] == 'r')
                 ent->perms |= MEM_PERM_READ;
@@ -213,12 +255,24 @@ static void parse_maps_ent(char *str, struct mem_map_entry *ent)
             if (token[2] == 'x')
                 ent->perms |= MEM_PERM_EXEC;
             break;
+        /*5.- PATHNAME*/
         case 5:
             ent->pathname = strdup(token);
             break;
         }
     }
 }
+
+/*Obtain the process memory*/
+/*
+ * What is exactly the process memory?
+ * Well, we are going to read from /proc/pid/map,
+ * we are going to parse all this memory and store it in a uint32_t
+ * array and then cast this array into a mem_map_entry, so we can return it.
+ * 
+ * in summary and making it easy, we parse all the /proc/pid/maps file into a
+ * mem_map_entry, in a cool way
+ */
 
 struct mem_map_entry *get_process_memory(pid_t pid)
 {
@@ -228,14 +282,17 @@ struct mem_map_entry *get_process_memory(pid_t pid)
     struct mem_map_entry *ret, *current;
     FILE *fp;
 
+    /*Of course, we are reading from /proc/pid/maps*/
     sprintf(buf, "/proc/%d/maps", pid);
     fp = fopen(buf, "r");
     if (!fp)
         return NULL;
 
+    /*save memory for the mem_map_entry of the process*/
     num = calloc(1, sizeof(struct mem_map_entry) * num_ents + sizeof(uint32_t));
     ret = current = (struct mem_map_entry *)((uint32_t *)num + 1);
 
+    /*loop while we can still be reading from the file*/
     for (i = 0; fgets(buf, sizeof(buf), fp); ++i, ++current)
     {
         if (i >= num_ents)
@@ -248,6 +305,8 @@ struct mem_map_entry *get_process_memory(pid_t pid)
 
         if (strchr(buf, '\n'))
             *strchr(buf, '\n') = 0;
+
+        /*Parse it*/
         parse_maps_ent(buf, current);
     }
 
@@ -255,12 +314,25 @@ struct mem_map_entry *get_process_memory(pid_t pid)
     return ret;
 }
 
+/*Check if we are inside a SO*/
+/*
+ * This is our last and easier function, here we 
+ * get the process memory and then iterates for every entry.
+ * 
+ * We also have the ìnstruction pointer of the process, so when we are in
+ * the same place that the instruction pointer we check if the pathname
+ * contains /lic or /usr/lib. 
+ * 
+ * That's all folks.
+ */
+
 int mommy_am_i_inside_a_SO(pid_t pid)
 {
     int well_am_i = 0;
     struct mem_map_entry *mem_map, *ptr;
     struct user_regs_struct regs;
 
+    /*Get the process memory*/
     mem_map = get_process_memory(pid);
 
     if (ptrace(PTRACE_GETREGS, pid,
@@ -270,12 +342,14 @@ int mommy_am_i_inside_a_SO(pid_t pid)
         return -1;
     }
 
+    /*iterate the process memory*/
     mem_map_foreach(mem_map, ptr)
     {
         uint64_t page_addr = (uint64_t)ptr->addr;
         if (regs.rip < page_addr || regs.rip >= page_addr + ptr->size)
             continue;
 
+        /*We are in a SO if we are inside /lib of /usr/lic directory :)*/
         if (ptr->pathname != NULL)
         {
             well_am_i = !strncmp(ptr->pathname, "/lib", 4) || !strncmp(ptr->pathname, "/usr/lib", 8);
--- ptrace.h
+++ ptrace.h
@@ -7,6 +7,14 @@
 
 #include <string.h>
 
+/**
+ * struct which contains a map_entry. It has
+ * 
+ * The memory address
+ * The size of the memory region mapped
+ * the permission
+ * the path of the memory
+ */
 struct mem_map_entry
 {
     void *addr;
@@ -15,11 +23,15 @@ struct mem_map_entry
     char *pathname;
 };
 
+/* For iterate a mem_map_entry, because we don't use a normal array,
+  you can notice just by seeing get_process_memory function :) */
 #define mem_map_foreach(ents, ptr) \
     for ((ptr) = (ents); (ptr) < (ents) + mem_map_length(ents); ++(ptr))
 
+/* Lenght of a mem_map_entry, for out special loop */
 #define mem_map_length(ents) *(((uint32_t *)ents) - 1)
 
+/* Definition of permissions */
 #define MEM_PERM_READ (1 << 2)
 #define MEM_PERM_WRITE (1 << 1)
 #define MEM_PERM_EXEC (1 << 0)
@@ -30,12 +42,50 @@ void getdata(pid_t child, long addr,
 void putdata(pid_t child, long addr,
              char *str, int len);
 
+/**
+ * gives you a free space address of a process, ideal for injecting and not change the process
+ * 
+ * @param pid pid of the process
+ * 
+ * @return the free space address
+ */
 long freespaceaddr(pid_t pid);
 
+/**
+ * allows to write in a process memory.
+ * 
+ * @param pid pid of the process we want to write inside
+ * @param addr address in which we are going to write (must be of the process we are injecting)
+ * @param src code we are goint to write
+ * @param n size of the src
+ * 
+ * @return number of bytes written of -1 if something went wrong
+ */
 int ptrace_writemem(pid_t pid, void *addr, void *src, size_t n);
+
+/**
+ * allows to read from a process memory.
+ * 
+ * @param pid pid of the process we want to read from
+ * @param addr address we are going to read
+ * @param buf is where the read code is going to be stored
+ * @param n how much we want to read
+ * 
+ * @return number of bytes we just read oe r1 if something went wrong
+ */
 
 int ptrace_readmem(pid_t pid, void *addr, void *buf, size_t n);
 
+
+/*Copied from narhem, he did a good work, check his stuff*/
+/************https://github.com/narhen/procjack***********/
+/**
+ * Check if we are in a SO
+ * 
+ * @param pid pid of the process we are checking
+ * 
+ * @return 1 if we are in a SO, 0 if we are not
+ */
 int mommy_am_i_inside_a_SO(pid_t pid);
 
 #endif
